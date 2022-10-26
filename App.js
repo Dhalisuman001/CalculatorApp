117ced778661076445d8013bd69d92214aec990f
Suman Dhali <dhalisuman001@gmail.com>
2022-10-26 13:14:08 +0530

    Calculator done

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { SafeAreaView, StyleSheet, Switch, Text, View } from "react-native";
+import MyKeyBoard from "./src/components/MyKeyBoard";
+import { ThemeContext } from "./src/context/ThemeContext";
+import myColors from "./src/styles/Color";
 
 export default function App() {
+  const [theme, setTheme] = useState("light");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeContext.Provider value={theme}>
+      <SafeAreaView
+        style={theme === "light" ? styles.containerLight : styles.containerDark}
+      >
+        <Switch
+          value={theme === "light"}
+          onValueChange={() => setTheme(theme === "light" ? "dark" : "light")}
+        />
+        <MyKeyBoard />
+      </SafeAreaView>
+    </ThemeContext.Provider>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  containerLight: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: myColors.light,
+    alignItems: "center",
+    justifyContent: "flex-start",
+  },
+  containerDark: {
+    flex: 1,
+    backgroundColor: myColors.dark,
+    alignItems: "center",
+    justifyContent: "flex-start",
   },
 });
